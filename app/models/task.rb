edfba91e780dead24ec522f641a9a4248ce7ca07
wratterman <will.ratterman@gmail.com>
2017-06-24 10:15:25 -0600

    added SQL tables to record tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,15 @@
+require 'sqlite3'
+
 class Task
+
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
+    @database    = SQLite3::Database.new('db/task_manager_development.db')
+    @database.results_as_hash = true
+  end
+
+  def save
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 end
